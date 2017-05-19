ecbca76982f65407ce138a3894df42e3c122a004
Enid516 <1578802364@qq.com>
2017-05-19 16:57:15 +0800

    server mvp 添加 接口方法

---
+++ app/src/main/java/com/enid/kwliving/server/presenter/IServerPresenter.java
@@ -0,0 +1,8 @@
+package com.enid.kwliving.server.presenter;
+
+/**
+ * Created by Enid on 2017/5/19.
+ */
+
+public interface IServerPresenter {
+}
---
+++ app/src/main/java/com/enid/kwliving/server/presenter/ServerPresenterImpl.java
@@ -0,0 +1,8 @@
+package com.enid.kwliving.server.presenter;
+
+/**
+ * Created by Enid on 2017/5/19.
+ */
+
+public class ServerPresenterImpl implements IServerPresenter {
+}
